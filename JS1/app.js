e727e0d870e82a491e70de7c970aaa20d6ef0e97
Omkar Deshpande <deshpandeomkar11@hotmail.com>
2023-09-07 18:31:57 +0530

    Deleted files from this location

--- JS1/app.js
+++ JS1/app.js
@@ -16,7 +16,7 @@ fruits.splice(2, 0, "grape")
 fruits.splice(1, 1, "pear")
 
 // 6. Print the final "fruits" array after performing the above operations.
-console.log(fruits)
+// console.log(fruits)
 
 //------------------------------------------------------------------------------------------------------------------------------
 
@@ -40,11 +40,12 @@ delete person.age
 // 4. Add a new property called "job" with the value "Engineer" to the "person" object.
 person.job = "Engineer"
 
+
 // 5. Update the "city" property of the "person" object to "San Francisco".
 person.city = "San Francisco"
 
 // 6. Print the final "person" object after performing the above operations.
-console.log(person)
+// console.log(person)
 
 
 //------------------------------------------------------------------------------------------------------------------------------
@@ -54,6 +55,7 @@ console.log(person)
 
 // 1. Create an empty array called "cars".
 let cars = []
+console.log(cars)
 
 // 2. Add three car objects to the "cars" array. Each car object should have the following properties:
 //    - make: "Toyota"
@@ -62,16 +64,18 @@ let cars = []
 cars.push({ make: "Toyota", model: "Camry", year: 2018 },
     { make: "Toyota", model: "Camry", year: 2018 },
     { make: "Toyota", model: "Camry", year: 2018 })
+console.log(cars)
+
 
 // 3. Remove the first car object from the "cars" array.
 cars.splice(0, 1)
-
+console.log(cars)
 // 4. Add a new car object to the "cars" array with the following properties:
 //    - make: "Honda"
 //    - model: "Civic"
 //    - year: 2020
 cars.push({ make: "Honda", model: "Civic", year: 2020 })
-
+console.log(cars)
 // 5. Update the "model" property of the second car object in the array to "Accord".
 cars[1].model = "Accord"
 
